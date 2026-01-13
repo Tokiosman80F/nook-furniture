f4503757e67c46be71c91c4429371ed81917c03d
Tokiosman80F <tokiosman96@gmail.com>
2026-01-13 18:21:09 +0600

    feat: resposive code added

--- Task-1-nook-furniture/script.js
+++ Task-1-nook-furniture/script.js
@@ -1,3 +1,27 @@
+const logo = document.querySelector(".product__logo");
+const badge = document.querySelector(".explore-now-badge");
+
+let rotation = 0;
+
+function animate() {
+  rotation += 0.5; // Speed: higher number = faster
+
+  // 1. Logo: Needs the translate(-50%, -50%) to stay centered
+  if (logo) {
+    logo.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+  }
+
+  // 2. Badge: Just needs the rotation (it is positioned by top/right)
+  if (badge) {
+    badge.style.transform = `rotate(${rotation}deg)`;
+  }
+
+  requestAnimationFrame(animate);
+}
+
+// Start the loop
+animate();
+
 const container = document.querySelector(".category__container");
 const prevBtn = document.querySelector(".category__scroll-button:first-child");
 const nextBtn = document.querySelector(".category__scroll-button:last-child");
@@ -83,3 +107,47 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// counter
+
+const counterBox = document.querySelector(".futura__counter-box");
+const counters = document.querySelectorAll(".futura__counter-number");
+const counterSpeed = 200; // Lower is faster
+
+const startCounters = () => {
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText.replace("+", ""); // Remove + for math
+
+      // Calculate the increment
+      const inc = target / counterSpeed;
+
+      if (count < target) {
+        // Round up and update the text
+        counter.innerText = Math.ceil(count + inc) + "+";
+        // Call function every 1ms for smoothness
+        setTimeout(updateCount, 1);
+      } else {
+        counter.innerText = target + "+";
+      }
+    };
+    updateCount();
+  });
+};
+
+// Intersection Observer to trigger when visible
+const observerOptions = {
+  threshold: 0.5, // Trigger when 50% of the box is visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      startCounters();
+      observer.unobserve(entry.target); // Stop observing once animated
+    }
+  });
+}, observerOptions);
+
+observer.observe(counterBox);
